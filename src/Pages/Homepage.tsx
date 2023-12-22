e38d642014afb112aff05b526d3cf81712525314
DylanLucas2001 <Dylan_Lucas2001@protonmail.com>
2023-12-22 14:44:21 +0100

    BackToDesktop

--- src/Pages/Homepage.tsx
+++ src/Pages/Homepage.tsx
@@ -1,23 +1,29 @@
-import React from 'react'
-import useFetch from '../hooks/useFetch'
-
-
+import React from "react";
+import useFetch from "../hooks/useFetch";
 
 export default function Homepage() {
-    const {loading, error, data} = useFetch('http://localhost:1337/api/products?populate=*')
+  const { loading, error, data } = useFetch(
+    "http://localhost:1338/api/products?populate=*"
+  );
 
-    if (loading) return <p>loading...</p>
-    if (error) return <p>error D:</p>
-    if (data) return (
-        <main>
-            {data.map(product => (
-                <div key={product.id}>
-                    <p>{product.attributes.name}</p>
-                    <img src={"http://localhost:1337" + product.attributes.productImage.data[0].attributes.url}/>
-                    <p>{product.attributes.body}</p>
-                </div>
-            ))}
-        </main>
-    )
-    return(null);
+  if (loading) return <p>loading...</p>;
+  if (error) return <p>error D:</p>;
+  if (data)
+    return (
+      <main>
+        {data.map((product) => (
+          <div key={product.id}>
+            <p>{product.attributes.name}</p>
+            <img
+              src={
+                "http://localhost:1338" +
+                product.attributes.productImage.data[0].attributes.url
+              }
+            />
+            <p>{product.attributes.body}</p>
+          </div>
+        ))}
+      </main>
+    );
+  return null;
 }
